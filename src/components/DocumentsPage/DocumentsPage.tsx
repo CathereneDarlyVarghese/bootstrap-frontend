9c241b7de24ce4839faa9682b19d8cd7b7993127
Naveen Joy <naveenjoy18@gmail.com>
2023-06-08 19:04:11 +0530

    documents page accepts props

--- src/components/DocumentsPage/DocumentsPage.tsx
+++ src/components/DocumentsPage/DocumentsPage.tsx
@@ -2,6 +2,12 @@ import React, { useState } from "react";
 import DocumentsCard from "./DocumentsCard";
 import AddDocumentsForm from "./AddDocumentsForm";
 
+const testDescription =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque feugiat quam et fringilla dictum. Nunc facilisis vitae augue a";
+
+const testNotes =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque feugiat quam et fringilla dictum. Nunc facilisis vitae augue a fermentum. Quisque vel feugiat diam. Cras commodo elementum euismod. Suspendisse eleifend nulla elementum nulla cursus malesuada. Cras dapibus ipsum vitae venenatis scelerisque. Pellentesque dictum ";
+
 const DocumentsPage = () => {
   const [addDocumentsOpen, setAddDocumentsOpen] = useState(false);
   return (
@@ -21,9 +27,28 @@ const DocumentsPage = () => {
           </button>
         </div>
         <div>
-          <DocumentsCard />
-          <DocumentsCard />
-          <DocumentsCard />
+          <DocumentsCard
+            documentName="Test Application"
+            documentDescription={testDescription}
+            documentType="Invoice"
+            startDate="10 Jan 2023"
+            endDate="12 Feb 2025"
+            documentNotes={testNotes}
+            fileStatus="file missing"
+            documentStatus="expired"
+            fileName="sample file"
+          />
+          <DocumentsCard
+            documentName="Test Application 2"
+            documentDescription={testDescription}
+            documentType="Contract"
+            startDate="10 Jan 2021"
+            endDate="12 Feb 2023"
+            documentNotes={testNotes}
+            fileStatus="File Uploaded"
+            documentStatus="active"
+            fileName="sample file"
+          />
         </div>
       </div>
       <div>
