fb1a13d02ee6ce4970580a2a99099777b6c31a12
Joel <joeljohnvarghese2@gmail.com>
2023-06-21 22:26:06 +0530

    'All Assets' button added, while displaying Asset Sections. Clicking on Bootstrap logo redirects to '/' instead of '/home'

--- src/components/LandingPage/ListsLayout.tsx
+++ src/components/LandingPage/ListsLayout.tsx
@@ -62,7 +62,8 @@ const ListsLayout = (props: any) => {
   );
   const [selectedAssetPlacement, setSelectedAssetPlacement] =
     useState<AssetPlacement>(defaultAssetPlacements[0]);
-  const [selectedAssetPlacementName, setSelectedAssetPlacementName] = useState<string>("");
+  const [selectedAssetPlacementName, setSelectedAssetPlacementName] =
+    useState<string>("");
 
   const handleAddWorkOrder = () => {
     setShowWorkOrderForm(true);
@@ -91,7 +92,7 @@ const ListsLayout = (props: any) => {
   const scrollLeft = () => {
     setScroll(true);
     if (scroll) {
-      document.getElementById("scrollFirst").scrollIntoView({
+      document.getElementById("scrollAll").scrollIntoView({
         inline: "start",
         behavior: "smooth",
         block: "nearest",
@@ -240,9 +241,10 @@ const ListsLayout = (props: any) => {
         );
 
         if (selectedAssetPlacementName === "") {
-          setSelectedAssetPlacementName(filteredFetchedAssetPlacements[0].placement_name);
+          setSelectedAssetPlacementName(
+            filteredFetchedAssetPlacements[0].placement_name
+          );
         }
-
       } catch (error) {
         console.log(error);
       }
@@ -325,19 +327,38 @@ const ListsLayout = (props: any) => {
               style={{ width: "75%" }}
             >
               <ul className="flex flex-row">
+                <li>
+                  <button
+                    className={`btn bg-transparent font-sans text-xs md:text-[10px] ${
+                      activeTab === -1
+                        ? "text-blue-900 dark:text-white border-b-blue-800 dark:border-b-white hover:border-b-blue-800 hover:dark:border-b-white font-bold"
+                        : "text-gray-500 dark:text-gray-400 font-normal"
+                    } normal-case w-24 p-0 border-transparent rounded-none hover:bg-transparent hover:border-transparent `}
+                    id="scrollAll"
+                    onClick={() => {
+                      setActiveTab(-1);
+                      setSelectedAssetSection(defaultAssetSections[0]);
+                      setSelectedAssetPlacementName("");
+                    }}
+                  >
+                    All Assets
+                  </button>
+                </li>
                 {assetSections.map((item, index) => (
                   <li>
                     <button
-                      className={`btn bg-transparent font-sans text-xs md:text-[10px] ${activeTab === index
-                        ? "text-blue-900 dark:text-white border-b-blue-800 dark:border-b-white hover:border-b-blue-800 hover:dark:border-b-white font-bold"
-                        : "text-gray-500 dark:text-gray-400 font-normal"
-                        } normal-case w-24 p-0 border-transparent rounded-none hover:bg-transparent hover:border-transparent `}
-                      id={`${index === assetSections.length - 1
-                        ? "scrollLast"
-                        : index === 0
+                      className={`btn bg-transparent font-sans text-xs md:text-[10px] ${
+                        activeTab === index
+                          ? "text-blue-900 dark:text-white border-b-blue-800 dark:border-b-white hover:border-b-blue-800 hover:dark:border-b-white font-bold"
+                          : "text-gray-500 dark:text-gray-400 font-normal"
+                      } normal-case w-24 p-0 border-transparent rounded-none hover:bg-transparent hover:border-transparent `}
+                      id={`${
+                        index === assetSections.length - 1
+                          ? "scrollLast"
+                          : index === 0
                           ? "scrollFirst"
                           : ""
-                        }`}
+                      }`}
                       onClick={() => {
                         setActiveTab(index);
                         setSelectedAssetSection(item);
@@ -361,7 +382,8 @@ const ListsLayout = (props: any) => {
           <div className="px-2">
             <select
               onChange={(e) => setSelectedAssetPlacementName(e.target.value)}
-              className="select select-sm md:select-xs bg-white dark:bg-gray-700 text-black dark:text-white mb-3 md:mt-2 border border-slate-300 dark:border-gray-600 w-full">
+              className="select select-sm md:select-xs bg-white dark:bg-gray-700 text-black dark:text-white mb-3 md:mt-2 border border-slate-300 dark:border-gray-600 w-full"
+            >
               {/* <option value="" hidden disabled selected>Select a Placement</option> */}
               {assetPlacements.map((placement) => (
                 <option
@@ -427,7 +449,7 @@ const ListsLayout = (props: any) => {
                 onClick={() => {
                   setSelectedAsset(asset);
                   setAssetId(asset.asset_id);
-                  setAddAssetOpen(false)
+                  setAddAssetOpen(false);
                   removeClass(
                     "#parent-element .asset-details-card",
                     "lg:hidden"
@@ -460,8 +482,8 @@ const ListsLayout = (props: any) => {
               <AddAssetForm
                 addAssetOpen={addAssetOpen}
                 setAddAssetOpen={() => {
-                  setAddAssetOpen(false)
-                  setSelectedAsset(null)
+                  setAddAssetOpen(false);
+                  setSelectedAsset(null);
                 }}
               />
             ) : (
