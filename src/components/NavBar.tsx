ab830f39c0f7ea30a931c422ce35134de0ce24fd
aZrael936 <haniatta1@gmail.com>
2023-07-25 16:24:36 +0530

    Fix

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -206,7 +206,7 @@ const NavBar = () => {
                 <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
               </svg>
             </label>
-            {open && (
+            {open && locations && locations.length > 0 && (
               <ul
                 tabIndex={0}
                 className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 dark:bg-gray-700 rounded-box w-52"
